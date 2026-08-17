3c89f86b9e09ae6b435ee428363ce27bc0789e9e
Samuel Giraldo <samuelgiraldoroldan51@gmail.com>
2026-08-17 18:04:54 -0500

    🎉 configuración inicial del proyecto con Fastify y TypeScript

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', async () => {
+  return { message: 'Hotel funcionando' };
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+
+    console.log('Servidor ejecutándose en http://localhost:3000');
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
