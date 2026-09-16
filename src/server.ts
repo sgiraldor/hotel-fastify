c52394e5609a0e861a0fe120e68d05f4c09926da
sgiraldor <samuelgiraldoroldan51@gmail.com>
2026-09-16 15:28:42 -0500

    ✨ ruta v2 para husped

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { AppDataSource } from './config/database';
 import { huespedRoutes } from './routes/huesped.routes';
 import { habitacionRoutes } from './routes/habitacion.routes';
 import { checkinRoutes } from './routes/checkin.routes';
+import { huespedV2Routes } from './routes/v2/huesped-v2.routes';
 
 const app = fastify();
 
@@ -11,13 +12,14 @@ app.register(huespedRoutes);
 app.register(habitacionRoutes);
 app.register(checkinRoutes);
 
+app.register(huespedV2Routes);
+
 app.get('/', async () => {
   return { message: 'Hotel funcionando' };
 });
 
 const start = async () => {
   try {
-
     await AppDataSource.initialize();
     console.log('Base de datos conectada');
 
@@ -25,11 +27,10 @@ const start = async () => {
 
     await app.listen({
       port,
-      host: '0.0.0.0'
+      host: '0.0.0.0',
     });
 
     console.log(`Servidor ejecutándose en el puerto ${port}`);
-
   } catch (error) {
     app.log.error(error);
     process.exit(1);
